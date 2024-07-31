9312ba0883cae8873e900ed6595d418279a85060
Camilobarre <barrecami@gmail.com>
2024-07-31 07:27:13 -0500

    feat: Se crea el método de agregar un empleado

--- Models/Empresa.cs
+++ Models/Empresa.cs
@@ -8,34 +8,42 @@ public class Empresa
 {
     public string? Nombre { get; set; }
     public string? Direccion { get; set; }
-    public List<Empleado> ListaEmpleados = new List<Empleado>();
+    public static List<Empleado> ListaEmpleados = new List<Empleado>();
 
     public Empresa(string nombre, string direccion)
     {
         this.Nombre = nombre;
         this.Direccion = direccion;
     }
-    public static Empleado PedirDatosEmpleado(){
+    public static Empleado PedirDatosEmpleado()
+    {
         Console.WriteLine("Ingresa los datos del Empleado");
-        
+
         Console.Write("Nombre: ");
-        string? nombre= Console.ReadLine();
+        string? nombre = Console.ReadLine();
 
         Console.Write("Apellido:");
-        string? apellido= Console.ReadLine();
+        string? apellido = Console.ReadLine();
 
         Console.Write("Número de Identificación:");
-        string? numeroDeIdentificacion= Console.ReadLine();
+        string? numeroDeIdentificacion = Console.ReadLine();
 
         Console.Write("Edad");
-        byte edad= byte.Parse(Console.ReadLine());
+        byte edad = byte.Parse(Console.ReadLine());
 
         Console.Write("Posición:");
-        string? posicion= Console.ReadLine();
+        string? posicion = Console.ReadLine();
 
         Console.Write("Salario");
-        double salario= double.Parse(Console.ReadLine());
+        double salario = double.Parse(Console.ReadLine());
 
         return new Empleado(nombre, apellido, numeroDeIdentificacion, edad, posicion, salario);
+    }
+
+    public static void AgregarEmpleado()
+    {
+        Empleado empleado = PedirDatosEmpleado();
+        ListaEmpleados.Add(empleado);
+        Console.WriteLine("Empleado agregado correctamente");
     }
 }
