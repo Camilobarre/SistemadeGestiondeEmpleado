34ade9e9aea5c153428c5fe3e69bb1abafff9f03
Camilobarre <barrecami@gmail.com>
2024-07-31 07:16:51 -0500

    feat: Se crea un método para pedirle los datos al empleado

--- Models/Empresa.cs
+++ Models/Empresa.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 namespace POO_Sistema_Gestion_Empleados.Models;
 public class Empresa
 {
-    public string Nombre { get; set; }
-    public string Direccion { get; set; }
+    public string? Nombre { get; set; }
+    public string? Direccion { get; set; }
     public List<Empleado> ListaEmpleados = new List<Empleado>();
+
+    public Empresa(string nombre, string direccion)
+    {
+        this.Nombre = nombre;
+        this.Direccion = direccion;
+    }
+    public static Empleado PedirDatosEmpleado(){
+        Console.WriteLine("Ingresa los datos del Empleado");
+        
+        Console.Write("Nombre: ");
+        string? nombre= Console.ReadLine();
+
+        Console.Write("Apellido:");
+        string? apellido= Console.ReadLine();
+
+        Console.Write("Número de Identificación:");
+        string? numeroDeIdentificacion= Console.ReadLine();
+
+        Console.Write("Edad");
+        byte edad= byte.Parse(Console.ReadLine());
+
+        Console.Write("Posición:");
+        string? posicion= Console.ReadLine();
+
+        Console.Write("Salario");
+        double salario= double.Parse(Console.ReadLine());
+
+        return new Empleado(nombre, apellido, numeroDeIdentificacion, edad, posicion, salario);
+    }
 }
