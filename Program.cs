8bc7e539b929060636c088d910afc430541a1586
Camilobarre <barrecami@gmail.com>
2024-07-31 08:16:03 -0500

    feat: Se crea un menú para poder ejecutar el CRUD

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using POO_Sistema_Gestion_Empleados.Models;
+
+var bandera = true;
+
+while (bandera)
+{
+    Empresa.ImprimirMenu();
+    var opcion = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 0:
+            // Salir del programa
+            Console.WriteLine("Hasta luego, vuelva pronto!");
+            bandera = false;
+            break;
+        case 1:
+            // Agregar Empleado
+            Empresa.PedirDatosEmpleado();
+            Empresa.AgregarEmpleado();
+            Empresa.PausarMenu();
+            break;
+        case 2:
+            // Mostrar Empleado
+            Empresa.MostrarEmpleadoPorCargo();
+            Empresa.PausarMenu();
+            break;
+        case 3:
+            // Actualizar Empleado
+            Empresa.ActualizarEmpleado();
+            Empresa.PausarMenu();
+            break;
+        case 4:
+            // Eliminar Empleado
+            Empresa.Eliminarempleado();
+            Empresa.PausarMenu();
+            break;
+        default:
+            Console.WriteLine("Opción inválida, intente nuevamente.");
+            break;
+    }
+}
